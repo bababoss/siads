697a3eaffd354d6f29b9b7449acb417b675189c8
bharatsush <suresheca2012@gmail.com>
2016-04-22 19:46:41 +0530

    update database

--- attendance/urls.py
+++ attendance/urls.py
@@ -3,8 +3,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from attendance import views
 
 urlpatterns = [
-    url(r'^attendance/$',views.Analysis_tableList.as_view()),
-    url(r'^attendance/(?P<pk>[0-9]+)/$',views.Analysis_tableDetail.as_view()),
+    url(r'^attendance/(?P<user_id>[0-9]+)/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$',views.Analysis_tableList.as_view()),
+    url(r'^attendance/detail/(?P<user_id>[0-9]+)/$',views.Analysis_tableDetail.as_view()),
+    url(r'^attendance_input/$',views.Attendance_tableList.as_view()),
+    url(r'^attendance_input/(?P<pk>[0-9]+)/$',views.Attendance_tableDetail.as_view()),
+    url(r'^index/$',views.index, name = 'index'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
