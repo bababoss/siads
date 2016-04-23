88b5db959bfddbcbd5a99fe98969706e7722052b
bharatsush <suresheca2012@gmail.com>
2016-04-23 11:43:14 +0530

    update

--- attendance/urls.py
+++ attendance/urls.py
@@ -5,7 +5,7 @@ from attendance import views
 urlpatterns = [
     url(r'^attendance/(?P<user_id>[0-9]+)/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$',views.Analysis_tableList.as_view()),
     url(r'^attendance/detail/(?P<user_id>[0-9]+)/$',views.Analysis_tableDetail.as_view()),
-    url(r'^attendance_input/$',views.Attendance_tableList.as_view()),
+    url(r'^attendance_input/(?P<user_id>[0-9]+)/$',views.Attendance_tableList.as_view()),
     url(r'^attendance_input/(?P<pk>[0-9]+)/$',views.Attendance_tableDetail.as_view()),
     url(r'^index/$',views.index, name = 'index'),
 ]
